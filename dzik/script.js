68fa1ebeb425d3efba6c0e1f6fc194197ab4b4a8
schAKura <alan.kuranowski@tm1.edu.pl>
2024-10-21 14:12:41 +0200

    modified:   dzik/script.js

--- dzik/script.js
+++ dzik/script.js
@@ -1,7 +1,45 @@
+var puntky = 0;
+var pedal = 0;
+var punktyH = document.querySelector("#punkty");
+
+function zwieranie(liczbbbb, digitabel) {
+    if (liczbbbb == digitabel) {
+        return true;
+    }
+    while (liczbbbb != 0) {
+        if (liczbbbb % 10 == digitabel) {
+            return true;
+        }
+        liczbbbb = Math.floor(liczbbbb / 10);
+    }
+    return false;
+}
+
 function trzepac() {
-    let liczba = Math.floor(Math.random() * 100 + 1);
+    if(pedal){
+        puntky--;
+    }punktyH.innerHTML = puntky;
+    var liczba = Math.floor(Math.random() * 100 + 1);
     let liczbaH = document.querySelector("#liczba");
     liczbaH.innerHTML = liczba;
+    pedal = 0;
+}
+
+function clicked(opcja) {
+    pedal = 1;
+    if(opcja){
+        if(liczba%7==0 || zwieranie(liczba, 7)){
+            puntky++;
+        }else{
+            puntky--;
+        }
+    }else{
+        if(liczba%7!=0 && !zwieranie(liczba, 7)){
+            puntky++;
+        }else{
+            puntky--;
+        }
+    }
 }
 
 let myInterval = setInterval(trzepac, Number(prompt("Podaj szybkość gry w sekundach!")) * 1000);
